f100ebc0e4ed99058b38a6e462b14c350f4297e9
xhieu2206 <xhieu94@gmail.com>
2021-08-21 02:45:17 +0700

    11. Understanding Sequelize

---
+++ 11.Understanding_Sequelize/project/controllers/shop.js
@@ -0,0 +1,144 @@
+const Product = require('../models/product');
+
+exports.getProducts = (req, res, next) => {
+  Product.findAll()
+    .then(products => {
+      res.render('shop/product-list', {
+        prods: products,
+        pageTitle: 'All Products',
+        path: '/products'
+      });
+    })
+    .catch(err => console.log(`CLOG "err": `, err));
+};
+
+exports.getProduct = (req, res) => {
+  const prodId = req.params["productId"];
+  Product.findByPk(prodId)
+    .then(product => {
+      res.render('shop/product-detail', {
+        product: product,
+        pageTitle: product.title,
+        path: '/products'
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.findAll()
+    .then(products => {
+      res.render('shop/index', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    })
+    .catch(err => console.log(`CLOG "err": `, err));
+};
+
+exports.getCart = (req, res) => {
+  req.user.getCart()
+    .then(cart => {
+      return cart.getProducts();
+    })
+    .then(products => {
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products,
+      });
+    })
+    .catch(err => console.log(`CLOG error while fetching the cart detail "err": `, err));
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body['productId'];
+  let fetchedCart;
+  let newQuantity = 1;
+  req.user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId }})
+    })
+    .then(products => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
+      }
+      return Product.findByPk(prodId)
+    })
+    .then(product => {
+      return fetchedCart.addProduct(product, {
+        through: {
+          quantity: newQuantity,
+        }
+      })
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(`CLOG "err": `, err))
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user.getCart()
+    .then(cart => {
+      return cart.getProducts({ where: { id: prodId }})
+    })
+    .then(products => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(`CLOG "err": `, err));
+};
+
+exports.getOrders = (req, res) => {
+  req.user.getOrders({ include: ['products']})
+    .then(orders => {
+      res.render('shop/orders', {
+        orders,
+        path: '/orders',
+        pageTitle: 'Your Orders'
+      });
+    })
+    .catch(err => console.log(`CLOG "err": `, err));
+};
+
+exports.postOrder = (req, res) => {
+  let fetchedCart;
+  req.user.getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts();
+    })
+    .then(products => {
+      req.user.createOrder()
+        .then(order => {
+          return order.addProducts(products.map(product => {
+            product.orderItem = {
+              quantity: product.cartItem.quantity,
+            };
+            return product;
+          }))
+        })
+        .catch(err => console.log(`CLOG "err": `, err));
+    })
+    .then(() => {
+      return fetchedCart.setProducts(null);
+    })
+    .then(() => {
+      res.redirect('/orders');
+    })
+    .catch(err => console.log(`CLOG "err": `, err));
+};
