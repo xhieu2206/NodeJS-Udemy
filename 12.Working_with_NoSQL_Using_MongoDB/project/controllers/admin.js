6af3f10fce8214542dfb90a80b1f762e1f67b276
xhieu2206 <xhieu94@gmail.com>
2021-09-01 02:39:34 +0700

    12. Working with NoSQL & Using MongoDB - Done Video 8

--- 12.Working_with_NoSQL_Using_MongoDB/project/controllers/admin.js
+++ 12.Working_with_NoSQL_Using_MongoDB/project/controllers/admin.js
@@ -26,149 +26,132 @@ exports.postAddProduct = (req, res, next) => {
     .catch(err => {
       console.log(`CLOG "err": `, err);
     });
-
-  /*
-    Sử dụng cách "truyền thống"
-   */
-  /*
-    Product.create({
-      title, imageUrl, price, description,
-      userId: req.user.id,
-    })
-      .then(result => {
-        console.log('Created Product');
-        res.redirect('/admin/products');
-      })
-      .catch(err => {
-        console.log(`CLOG "err": `, err);
-      });
-   */
 };
 
-exports.getEditProduct = (req, res) => {
-  const editMode = req.query["edit"];
-  if (!editMode) {
-    return res.redirect('/');
-  }
-  const prodId = req.params["productId"];
-  /*
-    Sử dụng method `getProducts` được Sequelize defined
-   */
-  req.user
-    .getProducts({
-      where: {
-        id: prodId
-      }
-    })
-    .then(products => {
-      if (!products[0]) {
-        return res.redirect('/');
-      }
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode,
-        product: products[0]
-      });
-    })
-    .catch(err => console.log(`CLOG error while getting the product with ID "${prodId}" "err": `, err));
-  /*
-    Product
-      .findByPk(prodId)
-      .then(product => {
-        if (!product) {
-          return res.redirect('/');
-        }
-        res.render('admin/edit-product', {
-          pageTitle: 'Edit Product',
-          path: '/admin/edit-product',
-          editing: editMode,
-          product: product
-        });
-      })
-      .catch(err => console.log(`CLOG error while getting the product with ID "${prodId}" "err": `, err));
-   */
-};
-
-exports.postEditProduct = (req, res) => {
-  const prodId = req.body['productId'];
-  const updatedTitle = req.body.title;
-  const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
-  const updatedDesc = req.body.description;
-  Product
-    .findByPk(prodId)
-    .then(p => {
-      if (!p) {
-        res.redirect('/admin/products');
-      } else {
-        p.title = updatedTitle;
-        p.price = updatedPrice;
-        p.imageUrl = updatedImageUrl;
-        p.description = updatedDesc;
-        return p.save();
-      }
-    })
-    .then(() => {
-      console.log(`CLOG "Updated product successfully": `, null);
-      res.redirect('/admin/products')
-    })
-    .catch(err => console.log(`CLOG "err": `, err))
-  /*
-    Product
-      .update({
-        title: updatedTitle,
-        imageUrl: updatedImageUrl,
-        description: updatedDesc,
-        price: updatedPrice
-      }, {
-        where: {
-          id: prodId
-        }
-      })
-      .then(() => {
-        res.redirect('/admin/products');
-      })
-      .catch(err => console.log(`CLOG error while updating the product with ID "${prodId}" "err": `, err));
-   */
-};
-
-exports.getProducts = (req, res) => {
-  req.user
-    .getProducts()
-    .then(products => {
-      res.render('admin/products', {
-        prods: products,
-        pageTitle: 'Admin Products',
-        path: '/admin/products'
-      });
-    })
-    .catch(err => console.log(`CLOG error when getting all products "err": `, err));
-
-  /*
-    Product
-      .findAll()
-      .then(products => {
-        res.render('admin/products', {
-          prods: products,
-          pageTitle: 'Admin Products',
-          path: '/admin/products'
-        });
-      })
-      .catch(err => console.log(`CLOG error when getting all products "err": `, err));
-   */
-};
-
-exports.postDeleteProduct = (req, res) => {
-  const prodId = req.body["productId"];
-  Product
-    .destroy({
-      where: {
-        id: prodId
-      }
-    })
-    .then(() => {
-      res.redirect('/admin/products');
-    })
-    .catch(err => console.log(`CLOG error while deleting the product with ID "${prodId}" "err": `, err));
-};
+// exports.getEditProduct = (req, res) => {
+//   const editMode = req.query["edit"];
+//   if (!editMode) {
+//     return res.redirect('/');
+//   }
+//   const prodId = req.params["productId"];
+//   /*
+//     Sử dụng method `getProducts` được Sequelize defined
+//    */
+//   req.user
+//     .getProducts({
+//       where: {
+//         id: prodId
+//       }
+//     })
+//     .then(products => {
+//       if (!products[0]) {
+//         return res.redirect('/');
+//       }
+//       res.render('admin/edit-product', {
+//         pageTitle: 'Edit Product',
+//         path: '/admin/edit-product',
+//         editing: editMode,
+//         product: products[0]
+//       });
+//     })
+//     .catch(err => console.log(`CLOG error while getting the product with ID "${prodId}" "err": `, err));
+//   /*
+//     Product
+//       .findByPk(prodId)
+//       .then(product => {
+//         if (!product) {
+//           return res.redirect('/');
+//         }
+//         res.render('admin/edit-product', {
+//           pageTitle: 'Edit Product',
+//           path: '/admin/edit-product',
+//           editing: editMode,
+//           product: product
+//         });
+//       })
+//       .catch(err => console.log(`CLOG error while getting the product with ID "${prodId}" "err": `, err));
+//    */
+// };
+//
+// exports.postEditProduct = (req, res) => {
+//   const prodId = req.body['productId'];
+//   const updatedTitle = req.body.title;
+//   const updatedPrice = req.body.price;
+//   const updatedImageUrl = req.body.imageUrl;
+//   const updatedDesc = req.body.description;
+//   Product
+//     .findByPk(prodId)
+//     .then(p => {
+//       if (!p) {
+//         res.redirect('/admin/products');
+//       } else {
+//         p.title = updatedTitle;
+//         p.price = updatedPrice;
+//         p.imageUrl = updatedImageUrl;
+//         p.description = updatedDesc;
+//         return p.save();
+//       }
+//     })
+//     .then(() => {
+//       console.log(`CLOG "Updated product successfully": `, null);
+//       res.redirect('/admin/products')
+//     })
+//     .catch(err => console.log(`CLOG "err": `, err))
+//   /*
+//     Product
+//       .update({
+//         title: updatedTitle,
+//         imageUrl: updatedImageUrl,
+//         description: updatedDesc,
+//         price: updatedPrice
+//       }, {
+//         where: {
+//           id: prodId
+//         }
+//       })
+//       .then(() => {
+//         res.redirect('/admin/products');
+//       })
+//       .catch(err => console.log(`CLOG error while updating the product with ID "${prodId}" "err": `, err));
+//    */
+// };
+//
+// exports.getProducts = (req, res) => {
+//   req.user
+//     .getProducts()
+//     .then(products => {
+//       res.render('admin/products', {
+//         prods: products,
+//         pageTitle: 'Admin Products',
+//         path: '/admin/products'
+//       });
+//     })
+//     .catch(err => console.log(`CLOG error when getting all products "err": `, err));
+//
+//   /*
+//     Product
+//       .findAll()
+//       .then(products => {
+//         res.render('admin/products', {
+//           prods: products,
+//           pageTitle: 'Admin Products',
+//           path: '/admin/products'
+//         });
+//       })
+//       .catch(err => console.log(`CLOG error when getting all products "err": `, err));
+//    */
+// };
+//
+// exports.postDeleteProduct = (req, res) => {
+//   const prodId = req.body["productId"];
+//   Product
+//     .destroy({
+//       where: {
+//         id: prodId
+//       }
+//     })
+//     .then(() => {
+//       res.redirect('/admin/products');
+//     })
+//     .catch(err => console.log(`CLOG error while deleting the product with ID "${prodId}" "err": `, err));
+// };
--- 12.Working_with_NoSQL_Using_MongoDB/project/util/database.js
+++ 12.Working_with_NoSQL_Using_MongoDB/project/util/database.js
@@ -1,8 +1,27 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', '1234', {
-    dialect: 'mysql',
-    host: 'localhost',
-});
+let _db; // the under_score để cho biết rằng biến này sẽ chỉ được dùng ở trong file này thôi
 
-module.exports = sequelize;
+const mongoConnect = (callback) => {
+  MongoClient.connect('mongodb+srv://xhieu2206:XNZEtGgJ$v6V7n2@nodejs-course.h1piu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+    .then(client => {
+      console.log('Connected');
+      _db = client.db()
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+}
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No Database Found!!!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
