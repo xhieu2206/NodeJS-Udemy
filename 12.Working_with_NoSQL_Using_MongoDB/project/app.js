ab27a23d558e88dd1d76ba77c950ceb32ca612f2
xhieu2206 <xhieu94@gmail.com>
2021-09-05 02:03:51 +0700

    12. Working with NoSQL & Using MongoDB - Done Video 19

--- 12.Working_with_NoSQL_Using_MongoDB/project/app.js
+++ 12.Working_with_NoSQL_Using_MongoDB/project/app.js
@@ -1,10 +1,11 @@
 const path = require('path');
-
+const PREDEFINED_USER_ID = '6133c0f037fa1a511db6d2fe';
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User
-  //   .findByPk(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(`CLOG "err": `, err))
-  next();
+  User.findById(PREDEFINED_USER_ID)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 })
 
 app.use('/admin', adminRoutes);
--- 12.Working_with_NoSQL_Using_MongoDB/project/models/user.js
+++ 12.Working_with_NoSQL_Using_MongoDB/project/models/user.js
@@ -1,22 +1,31 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const sequelize = require('../util/database');
-
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
   }
-});
+
+  save() {
+    const db = getDb();
+    return db.collection('users').insertOne(this);
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db.collection('users')
+      .findOne({ // sử dụng findOne, không cần dùng `next()` nữa
+        _id: new mongodb.ObjectId(userId)
+      })
+      .then(user => {
+        console.log(user);
+        return user;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+}
 
 module.exports = User;
