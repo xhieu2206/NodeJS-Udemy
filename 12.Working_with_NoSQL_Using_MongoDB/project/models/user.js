63013b3733882b93b06d94e7e82c703e2efbc076
xhieu2206 <xhieu94@gmail.com>
2021-09-05 02:12:33 +0700

    12. Working with NoSQL & Using MongoDB - Done Video 21

--- 12.Working_with_NoSQL_Using_MongoDB/project/models/user.js
+++ 12.Working_with_NoSQL_Using_MongoDB/project/models/user.js
@@ -2,14 +2,37 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class User {
-  constructor(name, email) {
+  constructor(name, email, cart, id) {
     this.name = name;
     this.email = email;
+    this.cart = cart; // { items: [] }
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     return db.collection('users').insertOne(this);
+  }
+
+  addToCart(product) {
+    const db = getDb();
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = {
+      items: [{
+        ...product,
+        quantity: 1
+      }]
+    }
+    return db.collection('users')
+      .updateOne({
+        _id: new mongodb.ObjectId(this._id)
+      }, {
+        $set: {
+          cart: updatedCart
+        }
+      });
   }
 
   static findById(userId) {
