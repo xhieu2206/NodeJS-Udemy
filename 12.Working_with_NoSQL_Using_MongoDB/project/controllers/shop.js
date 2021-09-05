f08ba4e516acc1b6f0aa4cd12c086286122bce31
xhieu2206 <xhieu94@gmail.com>
2021-09-05 16:49:17 +0700

    12. Working with NoSQL & Using MongoDB - DONE

--- 12.Working_with_NoSQL_Using_MongoDB/project/controllers/shop.js
+++ 12.Working_with_NoSQL_Using_MongoDB/project/controllers/shop.js
@@ -56,60 +56,22 @@ exports.postCart = (req, res, next) => {
       return req.user.addToCart(product);
     })
     .then(result => {
-      console.log(result);
+      res.redirect('/cart');
     })
     .catch(err => console.log(err));
-  // let fetchedCart;
-  // let newQuantity = 1;
-  // req.user
-  //   .getCart()
-  //   .then(cart => {
-  //     fetchedCart = cart;
-  //     return cart.getProducts({ where: { id: prodId }})
-  //   })
-  //   .then(products => {
-  //     let product;
-  //     if (products.length > 0) {
-  //       product = products[0];
-  //     }
-  //     if (product) {
-  //       const oldQuantity = product.cartItem.quantity;
-  //       newQuantity = oldQuantity + 1;
-  //       return product;
-  //     }
-  //     return Product.findByPk(prodId)
-  //   })
-  //   .then(product => {
-  //     return fetchedCart.addProduct(product, {
-  //       through: {
-  //         quantity: newQuantity,
-  //       }
-  //     })
-  //   })
-  //   .then(() => {
-  //     res.redirect('/cart');
-  //   })
-  //   .catch(err => console.log(`CLOG "err": `, err))
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  req.user.getCart()
-    .then(cart => {
-      return cart.getProducts({ where: { id: prodId }})
-    })
-    .then(products => {
-      const product = products[0];
-      return product.cartItem.destroy();
-    })
-    .then(result => {
+  req.user.deleteItemFromCart(prodId)
+    .then(() => {
       res.redirect('/cart');
     })
     .catch(err => console.log(`CLOG "err": `, err));
 };
 
 exports.getOrders = (req, res) => {
-  req.user.getOrders({ include: ['products']})
+  req.user.getOrders()
     .then(orders => {
       res.render('shop/orders', {
         orders,
@@ -121,27 +83,8 @@ exports.getOrders = (req, res) => {
 };
 
 exports.postOrder = (req, res) => {
-  let fetchedCart;
-  req.user.getCart()
-    .then(cart => {
-      fetchedCart = cart;
-      return cart.getProducts();
-    })
-    .then(products => {
-      req.user.createOrder()
-        .then(order => {
-          return order.addProducts(products.map(product => {
-            product.orderItem = {
-              quantity: product.cartItem.quantity,
-            };
-            return product;
-          }))
-        })
-        .catch(err => console.log(`CLOG "err": `, err));
-    })
-    .then(() => {
-      return fetchedCart.setProducts(null);
-    })
+  req.user
+    .addOrder()
     .then(() => {
       res.redirect('/orders');
     })
