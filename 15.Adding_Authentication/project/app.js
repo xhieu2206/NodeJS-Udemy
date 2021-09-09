8662b7fabac82c00fd0d9754ffa5f78a2ccecac7
xhieu2206 <xhieu94@gmail.com>
2021-09-10 01:15:40 +0700

    15. Adding Authentication - Done video 4

---
+++ 15.Adding_Authentication/project/app.js
@@ -0,0 +1,87 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+const MONGODB_URI = 'mongodb+srv://xhieu2206:XNZEtGgJ$v6V7n2@nodejs-course.h1piu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PRE_DEFINED_USER_ID = '613648d6837d38074d53a701';
+
+const app = express();
+/* setup for saving session in mongodb database */
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'session',
+})
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoute = require('./routes/auth');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'my secret',
+  resave: false,
+  saveUninitialized: false,
+  store
+}));
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User
+    .findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+  User.findById(PRE_DEFINED_USER_ID)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoute);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Xuan Hieu',
+          email: 'xhieu94@gmail.com',
+          cart: {
+            items: []
+          }
+        });
+        return user.save();
+      }
+    });
+  })
+  .then(() => {
+    console.log('Connected!!!')
+    app.listen(4000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
---
+++ 15.Adding_Authentication/project/controllers/auth.js
@@ -0,0 +1,46 @@
+const User = require('../models/user');
+const PRE_DEFINED_USER_ID = '613648d6837d38074d53a701';
+
+exports.getLogin = (req, res, next) => {
+  // const isLoggedIn = req.get('Cookie').split('; ')[2].split('=')[1] === 'true';
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false,
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  /* default thì cookie sẽ được send cùng với mọi request, không chỉ nhận được mà còn được gửi đi, có thể tham khảo thêm các value khác */
+  /* sử dụng session thay cho cookie
+    res.setHeader('Set-Cookie', 'loggedIn=true');
+   */
+  User
+    .findById(PRE_DEFINED_USER_ID)
+    .then(user => {
+      req.session.user = user;
+      req.session.isLoggedIn = true;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  })
+};
