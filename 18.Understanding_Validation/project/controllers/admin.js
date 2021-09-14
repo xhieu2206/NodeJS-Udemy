8a1189b4a07397cd83e94c7002ae7c15e42f2591
xhieu2206 <xhieu94@gmail.com>
2021-09-15 00:52:20 +0700

    18. Understanding Validation - DONE

--- 18.Understanding_Validation/project/controllers/admin.js
+++ 18.Understanding_Validation/project/controllers/admin.js
@@ -1,3 +1,4 @@
+const { validationResult } = require('express-validator');
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
@@ -5,10 +6,13 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
     editing: false,
+    hasError: false,
+    errorMessage: null,
+    validationErrors: [],
   });
 };
 
-exports.postAddProduct = (req, res, next) => {
+exports.postAddProduct = (req, res) => {
   /* đây là một cách để protecting routes, tuy nhiên cách này rất 'cồng kềnh' và RYS, do đó chúng ta sẽ sử dụng một thứ gọi là middleware
     if (!req.session.isLoggedIn) {
       return res.redirect('/login');
@@ -18,6 +22,20 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      editing: false,
+      hasError: true,
+      product: { title, imageUrl, price, description },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
+    });
+  }
+
   const product = new Product({
     title: title,
     price: price,
@@ -27,8 +45,7 @@ exports.postAddProduct = (req, res, next) => {
   });
   product
     .save()
-    .then(result => {
-      // console.log(result);
+    .then(() => {
       console.log('Created Product');
       res.redirect('/admin/products');
     })
@@ -53,6 +70,9 @@ exports.getEditProduct = (req, res, next) => {
         path: '/admin/edit-product',
         editing: editMode,
         product: product,
+        hasError: false,
+        errorMessage: null,
+        validationErrors: [],
       });
     })
     .catch(err => console.log(err));
@@ -64,6 +84,25 @@ exports.postEditProduct = (req, res) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: true,
+      hasError: true,
+      product: {
+        title: updatedTitle,
+        imageUrl: updatedImageUrl,
+        price: updatedPrice,
+        description: updatedDesc,
+        _id: prodId,
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
+    });
+  }
 
   Product.findById(prodId)
     .then(product => {
--- 18.Understanding_Validation/project/routes/admin.js
+++ 18.Understanding_Validation/project/routes/admin.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const express = require('express');
-
+const { body } = require('express-validator');
 const adminController = require('../controllers/admin');
 
 const router = express.Router();
@@ -9,17 +9,32 @@ const isAuth = require('../middleware/is-auth');
 
 // /admin/add-product => GET
 /* traveler từ left to right, do đó chúng ta có thể put middleware vào giữa */
-router.get('/add-product', isAuth, adminController.getAddProduct);
+router.get('/add-product', [
+  body('title').isAlphanumeric().isLength({ min: 3 }).trim(),
+  body('imageUrl').isURL(),
+  body('price').isFloat(),
+  body('description').trim().isLength({ min: 5, max: 200 })
+], isAuth, adminController.getAddProduct);
 
 // /admin/products => GET
 router.get('/products', isAuth, adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', isAuth, adminController.postAddProduct);
+router.post('/add-product', [
+  body('title').isString().isLength({ min: 3 }).trim(),
+  body('imageUrl').isURL(),
+  body('price').isFloat(),
+  body('description').trim().isLength({ min: 5, max: 200 })
+], isAuth, adminController.postAddProduct);
 
 router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
 
-router.post('/edit-product', isAuth, adminController.postEditProduct);
+router.post('/edit-product', [
+  body('title').isString().isLength({ min: 3 }).trim(),
+  body('imageUrl').isURL(),
+  body('price').isFloat(),
+  body('description').trim().isLength({ min: 5, max: 200 })
+], isAuth, adminController.postEditProduct);
 
 router.post('/delete-product', isAuth, adminController.postDeleteProduct);
 
