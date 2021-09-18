e6fd6838d8c7f021f17daaba9d3a8f9317db388a
xhieu2206 <xhieu94@gmail.com>
2021-09-19 02:43:15 +0700

    21. Adding Pagination - DONE

---
+++ 21.Adding_Pagination/project/routes/admin.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+const express = require('express');
+const { body } = require('express-validator');
+const adminController = require('../controllers/admin');
+
+const router = express.Router();
+const isAuth = require('../middleware/is-auth');
+
+// /admin/add-product => GET
+/* traveler từ left to right, do đó chúng ta có thể put middleware vào giữa */
+router.get('/add-product', isAuth, adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', isAuth, adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', [
+  body('title').isString().isLength({ min: 3 }).trim(),
+  body('price').isFloat(),
+  body('description').trim().isLength({ min: 5, max: 200 })
+], isAuth, adminController.postAddProduct);
+
+router.get('/edit-product/:productId', isAuth, adminController.getEditProduct);
+
+router.post('/edit-product', [
+  body('title').isString().isLength({ min: 3 }).trim(),
+  body('price').isFloat(),
+  body('description').trim().isLength({ min: 5, max: 200 })
+], isAuth, adminController.postEditProduct);
+
+router.post('/delete-product', isAuth, adminController.postDeleteProduct);
+
+module.exports = router;
