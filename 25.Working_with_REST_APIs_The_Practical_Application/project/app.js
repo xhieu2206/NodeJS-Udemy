fa4012c843b8950e2b33fa7917f52949a79af85e
xhieu2206 <xhieu94@gmail.com>
2021-09-21 01:42:31 +0700

    25. Working with REST APIs - The Practical Application - Done video 10

--- 25.Working_with_REST_APIs_The_Practical_Application/project/app.js
+++ 25.Working_with_REST_APIs_The_Practical_Application/project/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const MONGODB_URI = process.env.MONGODB_URI;
 const feedRoutes = require('./routes/feed');
@@ -8,6 +9,7 @@ const feedRoutes = require('./routes/feed');
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')))
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
@@ -16,6 +18,13 @@ app.use((req, res, next) => {
 })
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message });
+})
 
 mongoose
   .connect(MONGODB_URI)
